c169c669a343a10b08e8a23a849ec99f68d87eef
Hannes Reinecke <hare@suse.de>
2011-09-09 14:35:40 +0200

    dm-ssdcache: Remove merge function
    Currently we don't actually support I/O larger than the blocksize.
    Which happens to be the system pagesize. So the merge function
    will do more harm than good.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/md/dm-ssdcache.c
+++ drivers/md/dm-ssdcache.c
@@ -1439,21 +1439,6 @@ static int ssdcache_status(struct dm_target *ti, status_type_t type,
 	return 0;
 }
 
-static int ssdcache_merge(struct dm_target *ti, struct bvec_merge_data *bvm,
-			struct bio_vec *biovec, int max_size)
-{
-	struct ssdcache_ctx *sc = ti->private;
-	struct request_queue *q = bdev_get_queue(sc->target_dev->bdev);
-
-	if (!q->merge_bvec_fn)
-		return max_size;
-
-	bvm->bi_bdev = sc->target_dev->bdev;
-	bvm->bi_sector = dm_target_offset(ti, bvm->bi_sector);
-
-	return min(max_size, q->merge_bvec_fn(q, bvm, biovec));
-}
-
 static int ssdcache_iterate_devices(struct dm_target *ti,
 				  iterate_devices_callout_fn fn, void *data)
 {
@@ -1471,7 +1456,6 @@ static struct target_type ssdcache_target = {
 	.map    = ssdcache_map,
 	.end_io = ssdcache_endio,
 	.status = ssdcache_status,
-	.merge  = ssdcache_merge,
 	.iterate_devices = ssdcache_iterate_devices,
 };
 
